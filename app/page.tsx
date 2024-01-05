db3d1c4258ac233d461806f2e6207c4b1d18d4d9
Denys <levchenkoden02@gmail.com>
2024-01-05 20:40:37 +0200

    Init Next.js project

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Next.js Learn Courses
             </a>
             , brought to you by Vercel.
           </p>
