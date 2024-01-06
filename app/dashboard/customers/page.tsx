82031f378d8f5e5515ccd0d631ecc4e2dd72ca60
Denys <levchenkoden02@gmail.com>
2024-01-06 11:51:36 +0200

    Add new pages

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Customers Page</div>
+  )
+}
+
+export default page
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -10,6 +10,7 @@ export default function SideNav() {
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
         href="/"
       >
+          
         <div className="w-32 text-white md:w-40">
           <AcmeLogo />
         </div>
